ccf604dc62442bef16c998339198f4afd39e8a9e
monika <monika0846@gmail.com>
2017-08-31 10:14:13 -0700

    newbranchCreated

--- Mypract/src/NumberorNot.java
+++ Mypract/src/NumberorNot.java
@@ -22,7 +22,7 @@ import java.util.Scanner;
 	
 	    public static void main(String[] args)
 	    {
-	        System.out.println("Enter your mobile number");
+	        System.out.println("Enter your mobile number is");
 	 
 	        Scanner sc = new Scanner(System.in);
 	 
